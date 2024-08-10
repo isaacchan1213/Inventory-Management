6bff7fdf005cd3331d2b615463d7df529f288de9
Isaac Chan <isaacchan1213@gmail.com>
2024-08-10 04:17:58 -0400

    hotfix

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import {getAuth} from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "inventory-management-be174.firebaseapp.com",
   projectId: "inventory-management-be174",
   storageBucket: "inventory-management-be174.appspot.com",
